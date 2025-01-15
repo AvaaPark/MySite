e8c83e41de6b0406f65c948e5dc2df10d808543c
Rebecca Park <Rebecca Park>
2025-01-15 14:49:05 -0500

    Add bolding for navigation

--- ava-website/src/app/ui/navigation.js
+++ ava-website/src/app/ui/navigation.js
@@ -1,4 +1,8 @@
+'use client';
+
 import Link from 'next/link';
+import clsx from 'clsx';
+import { usePathname } from 'next/navigation';
 import Logo from '@/app/ui/logo';
 import styles from '@/app/styles/navigation.module.css';
 
@@ -6,26 +10,30 @@ import styles from '@/app/styles/navigation.module.css';
 const links = [
     {
         name: "About",
-        href: "about",
+        href: "/about",
     },
     {
         name: "Work",
-        href: "work"
+        href: "/work"
     },
     {
         name: "Fun",
-        href: "fun"
+        href: "/fun"
     }
 ]
 
 export default function Navigation() {
+    const path = usePathname();
+    console.log('path: ', path);
     return(
         <div className="flex mb-5">
             <Logo />
             <div className={`flex ${styles.navigation}`}>
                 {links.map(link => (
                     <Link
-                        className={styles.menu}
+                        className={clsx(styles.menu, {
+                            "font-semibold": path === link.href    
+                        })}
                         key={link.name}
                         href={link.href}
                     >
