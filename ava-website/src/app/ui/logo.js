c2998825152589e885bfd506a3154d7b1f9be691
Rebecca Park <Rebecca Park>
2025-01-15 14:27:24 -0500

    gsap test successful

--- ava-website/src/app/ui/logo.js
+++ ava-website/src/app/ui/logo.js
@@ -1,8 +1,18 @@
+'use client'
+
 import styles from '@/app/styles/logo.module.css';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 
 export default function Logo() {
+    useGSAP(() => {
+        gsap.to("#logo", {
+            rotation: "+=360"
+        })
+    });
+    
     return(
-        <div className={styles.logo}>
+        <div id="logo" className={`${styles.logo}`}>
             <p>Ava<br/>Park</p>
         </div>);
 };
